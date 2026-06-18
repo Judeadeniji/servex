493ea3ce483734f9fb1d71dc839999f3c4a8de65
judeadeniji <judeadeniji66@gmail.com>
2026-06-18 23:23:08 +0100

    fix: resolve global scope concurrency bugs by strictly isolating request context

--- src/scope.ts
+++ src/scope.ts
@@ -1,12 +1,8 @@
-import type { Context } from "./context";
 import type { RouterAdapter } from "./router/adapter";
 import type { ServerRoute } from "./types";
 
-let currentScope: Scope<ServerRoute[]> | null = null;
-
 class Scope<T extends ServerRoute[], P extends ServerRoute[] = ServerRoute[]> {
   router: RouterAdapter<T>;
-  context: Context = null!;
   parent: Scope<P> | null;
 
   constructor(rm: RouterAdapter<T>,
@@ -17,27 +13,8 @@ class Scope<T extends ServerRoute[], P extends ServerRoute[] = ServerRoute[]> {
   }
 }
 
-export function getCurrentScope<T extends ServerRoute[], P extends ServerRoute[] = ServerRoute[]>() {
-  if (currentScope === null) {
-    throw new Error("No scope found");
-  }
-  return currentScope as Scope<T, P>;
-}
-
 export function createScope<T extends any[]>(rm: RouterAdapter<T>) {
-  const scope = new Scope<T>(rm);
-
-  currentScope = scope;
-
-  return scope;
-}
-
-export function setScope<T extends any[]>(scope: Scope<T>) {
-  currentScope = scope
-}
-
-export function disposeScope() {
-  currentScope = null;
+  return new Scope<T>(rm);
 }
 
 export {
